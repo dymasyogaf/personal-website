b553e8992df9ee87695d9017b7577a4efb8e19dd
Dymas <dymas4464@gmail.com>
2025-10-18 23:11:12 +0700

    json paling apdet

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ⚡ ini yang mengaktifkan static export
+  images: {
+    unoptimized: true, // biar gambar gak error waktu export
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
